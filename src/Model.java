887c7a592485a44b9f40f53a0ffd9cd84891cd60
Chris <Chris@localhost>
2014-04-30 15:30:23 -0700

    Small Changes in model

--- src/Model.java
+++ src/Model.java
@@ -1,5 +1,4 @@
 
-
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -8,97 +7,76 @@ import java.net.URL;
 
 import com.google.gson.*;
 
-public class Model
-{
-	
+public class Model {
+
 	/*
-	 * Loren's Key: 4a505977e3e86a2c
-	 * Chris's Key: 0757ee8cfe589f60
+	 * Loren's Key: 4a505977e3e86a2c Chris's Key: 0757ee8cfe589f60
 	 */
 	private final String ACCESS_TOKEN = "4a505977e3e86a2c";
-	
 	private JsonElement jse = null;
 
+	public void getInfo(String zipcode) {
 
-public void getInfo(String zipcode)
-{
-	
-try
-{
-// Encode given URL -- could throw UnsupportedEncodingException
-String zip = URLEncoder.encode(zipcode, "utf-8");
+		try 
+		{
+			// Encode given URL -- could throw UnsupportedEncodingException
+			String zip = URLEncoder.encode(zipcode, "utf-8");
 
-// Construct Weather API URL
-URL weatherURL = new URL("http://api.wunderground.com/api/" + ACCESS_TOKEN + "/conditions/q/"+ zip +".json");
+			// Construct Weather API URL
+			URL weatherURL = new URL("http://api.wunderground.com/api/"
+					+ ACCESS_TOKEN + "/conditions/q/" 
+					+ zip + ".json");
 
-// Open the URL
-InputStream is = weatherURL.openStream(); // throws an IOException
-BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			// Open the URL
+			InputStream is = weatherURL.openStream(); // throws an IOException
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
-// Read the result into a JSON Element
-jse = new JsonParser().parse(br);
+			// Read the result into a JSON Element
+			jse = new JsonParser().parse(br);
 
-// Close the connection
-is.close();
-br.close();
-}
-catch (java.io.UnsupportedEncodingException uee)
-{
-uee.printStackTrace();
-}
-catch (java.net.MalformedURLException mue)
-{
-mue.printStackTrace();
-}
-catch (java.io.IOException ioe)
-{
-ioe.printStackTrace();
-}
+			// Close the connection
+			is.close();
+			br.close();
+			
+		} catch (java.io.UnsupportedEncodingException uee) {
+			uee.printStackTrace();
+		} catch (java.net.MalformedURLException mue) {
+			mue.printStackTrace();
+		} catch (java.io.IOException ioe) {
+			ioe.printStackTrace();
+		}
 
-}
-
-
-public String getWeather()
-{
-	if ( jse != null)
-	{
-       String weather = jse.getAsJsonObject().get("current_observation")
-                .getAsJsonObject().get("weather").getAsString();
-       return weather;
 	}
-	else
-	{
-		return null;
-	}
-}
 
-public double getTempurature()
-{
-	if (jse != null)
-	{
-      double temp = jse.getAsJsonObject().get("current_observation")
-                .getAsJsonObject().get("temp_f").getAsDouble();
-      return temp;
+	public String getWeather() {
+		if (jse != null) {
+			String weather = jse.getAsJsonObject().get("current_observation")
+					.getAsJsonObject().get("weather").getAsString();
+			return weather;
+		} else {
+			return null;
+		}
 	}
-	else 
-    {
-		Double temp = Double.NaN;
-		return temp;
-    }
-}
 
-public double getWindSpeed()
-{
-	if (jse != null)
-	{
-       double windmph = jse.getAsJsonObject().get("current_observation")
-                .getAsJsonObject().get("wind_mph").getAsDouble();
-       return windmph;
+	public double getTemperature() {
+		if (jse != null) {
+			double temp = jse.getAsJsonObject().get("current_observation")
+					.getAsJsonObject().get("temp_f").getAsDouble();
+			return temp;
+		} else {
+			Double temp = Double.NaN;
+			return temp;
+		}
 	}
-	else
-	{
-		Double windmph = Double.NaN;
-		return windmph;
+
+	public double getWindSpeed() {
+		if (jse != null) {
+			double windmph = jse.getAsJsonObject().get("current_observation")
+					.getAsJsonObject().get("wind_mph").getAsDouble();
+			return windmph;
+		} else {
+			Double windmph = Double.NaN;
+			return windmph;
+		}
 	}
-}
 }
