122eda45bff39926fc9d0c8e1f2cd8bdf6a05ec6
Chris Mendes <Chris Mendes@localhost>
2014-05-07 15:47:58 -0700

    Radar shows up

--- src/RadarPanel.java
+++ src/RadarPanel.java
@@ -1,35 +1,44 @@
-import javax.swing.JPanel;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import java.net.URL;
+import java.awt.*;
 
+import javax.swing.*;
+
+import acm.graphics.GCanvas;
 
 @SuppressWarnings("serial")
 public class RadarPanel extends JPanel implements WeatherPanel {
 
-	private WundergroundModel m;
+	private URL radarURL = null;
+
+	public RadarPanel() {
+		GCanvas canvas = new GCanvas();
+		canvas.setPreferredSize(new Dimension(500, 200));
+		this.add(canvas);
+
+		//This needs to get the URL from the WundergroundModel
+		try {
+			radarURL = new URL(
+					"http://api.wunderground.com/api/0757ee8cfe589f60/animatedradar/q/95678.gif?width=500&height=200&newmaps=1&timelabel=1&timelabel.y=10&num=15&delay=25");
+		} catch (java.net.MalformedURLException mue) {
+			mue.printStackTrace();
+		}
+
+		Icon icon = new ImageIcon(radarURL);
+		JLabel radarLabel = new JLabel(icon);
+
+		canvas.add(radarLabel, 0, 0);
+		canvas.setVisible(true);
+
+	}
+
 	@Override
 	public void onLocationChanged(Model newModel) {
 		// This method will be called when a new location has been retrieved.
 	}
-	
+
 	/*
-	 * You need to get the URL from WundergroundModel getRadar()
-	 * This Should work...
-	 *
-	public void showRadar()
-	{
-	    URL radarURL = m.getRadar();
-		Icon icon = new ImageIcon(radarURL);
-	    JLabel label = new JLabel(icon);
-	    JFrame rad = new JFrame("Radar");
-	    rad.getContentPane().add(label);
-	    rad.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    rad.pack();
-	    rad.setLocationRelativeTo(null);
-	    rad.setVisible(true);
-	}*/
+	 * You need to get the URL from WundergroundModel getRadar() This Should
+	 * work...
+	 */
 
 }
