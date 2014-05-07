cc758897c3076fc16b8e548e313d44e78a2e9b9f
Dalton247 <Dalton247@localhost>
2014-05-07 16:16:46 -0700

    Radar set up but with URL Trouble

--- src/RadarPanel.java
+++ src/RadarPanel.java
@@ -9,22 +9,18 @@ import acm.graphics.GCanvas;
 public class RadarPanel extends JPanel implements WeatherPanel {
 
 	private URL radarURL = null;
-
+	private JLabel radarLabel; 
+	
 	public RadarPanel() {
 		GCanvas canvas = new GCanvas();
 		canvas.setPreferredSize(new Dimension(500, 200));
 		this.add(canvas);
 
-		//This needs to get the URL from the WundergroundModel
-		try {
-			radarURL = new URL(
-					"http://api.wunderground.com/api/0757ee8cfe589f60/animatedradar/q/95678.gif?width=500&height=200&newmaps=1&timelabel=1&timelabel.y=10&num=15&delay=25");
-		} catch (java.net.MalformedURLException mue) {
-			mue.printStackTrace();
-		}
-
-		Icon icon = new ImageIcon(radarURL);
-		JLabel radarLabel = new JLabel(icon);
+		
+			
+		
+		
+		radarLabel = new JLabel();
 
 		canvas.add(radarLabel, 0, 0);
 		canvas.setVisible(true);
@@ -33,9 +29,14 @@ public class RadarPanel extends JPanel implements WeatherPanel {
 
 	@Override
 	public void onLocationChanged(Model newModel) {
-		// This method will be called when a new location has been retrieved.
-	}
+		
+		radarLabel.setIcon(new ImageIcon(newModel.getRadar()));
+			
 
+			
+			
+			
+	}
 	/*
 	 * You need to get the URL from WundergroundModel getRadar() This Should
 	 * work...
