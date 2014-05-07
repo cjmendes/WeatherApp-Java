f4737746171e24a72424973b4870178c2deee435
Nick Badal <badalnick@gmail.com>
2014-05-07 15:24:34 -0700

    Add temp to forecast columns

--- src/ForecastRow.java
+++ src/ForecastRow.java
@@ -1,20 +1,46 @@
+import acm.gui.HPanel;
 import acm.gui.VPanel;
 
 import javax.swing.*;
+import java.awt.*;
 
 @SuppressWarnings("serial")
 public class ForecastRow extends VPanel {
 		
 	private JLabel dayLabel;
-	
-	public ForecastRow() {
+    private JLabel lowLabel;
+    private JLabel highLabel;
+
+	public ForecastRow(int position) {
+        if(position % 2 == 0) {
+            setBackground(Color.WHITE);
+        } else {
+            setBackground(Color.LIGHT_GRAY);
+        }
+
 		dayLabel = new JLabel();
+        dayLabel.setHorizontalAlignment(JLabel.CENTER);
 		add(dayLabel);
 
+        HPanel tempArea = new HPanel();
+        tempArea.setOpaque(false);
+        lowLabel = new JLabel();
+        lowLabel.setHorizontalAlignment(JLabel.CENTER);
+        lowLabel.setForeground(Color.blue);
+        tempArea.add(lowLabel);
+        tempArea.add(new JLabel("|"));
+        highLabel = new JLabel();
+        highLabel.setHorizontalAlignment(JLabel.CENTER);
+        highLabel.setForeground(Color.red);
+        tempArea.add(highLabel);
+        add(tempArea);
 	}
 	
 	public void update(int position, Model model) {
 		// Update the various components here.
-        dayLabel.setText(model.getForecast().getDay(position));
-	}		
+        ForecastModel f = model.getForecast();
+        dayLabel.setText(f.getDay(position));
+        lowLabel.setText(""+f.getDayLow(position));
+        highLabel.setText(""+f.getDayHigh(position));
+	}
 }
