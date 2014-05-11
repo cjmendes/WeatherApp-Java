9b31a0793134a850b2c82a7efc8a1ca23aa64a73
Nick Badal <badalnick@gmail.com>
2014-05-11 11:01:18 -0700

    Rename forecast row to column (because its not a row, whoops)

--- src/ForecastPanel.java
+++ src/ForecastPanel.java
@@ -9,11 +9,11 @@ public class ForecastPanel extends JPanel implements WeatherPanel {
 	
 	private static final int NUM_COLS = 7;
 	
-	private ForecastRow[] days = new ForecastRow[NUM_COLS];
+	private ForecastColumn[] days = new ForecastColumn[NUM_COLS];
 	
 	public ForecastPanel() {
 		for(int i = 0; i < NUM_COLS; i++) {
-			days[i] = new ForecastRow(i);
+			days[i] = new ForecastColumn(i);
 			add(days[i]);
 		}
 		
