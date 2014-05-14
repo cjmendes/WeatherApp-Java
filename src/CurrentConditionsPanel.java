891d4cd05593f2eaefbe86c746ee07f33a30965c
Derek <Derek@localhost>
2014-05-13 23:43:56 -0700

    Removed old/useless comments in current conditions panel and moved jlabels around to make them look nice.

--- src/CurrentConditionsPanel.java
+++ src/CurrentConditionsPanel.java
@@ -11,7 +11,6 @@ public class CurrentConditionsPanel extends JPanel implements WeatherPanel
 	
 	private JLabel currentTemp, highTemp, lowTemp, conditionIcon, conditionText;
 	private JLabel precipPercent, feelsLike, humidity, elevation, windDir, windSpeed;
-	//Assigns current temp to variable cT so that it may be converted to an int
 	public CurrentConditionsPanel()
 	{
 		
@@ -20,19 +19,15 @@ public class CurrentConditionsPanel extends JPanel implements WeatherPanel
 		canvas.setPreferredSize(new Dimension(WeatherPanel.WIDTH, WeatherPanel.HEIGHT));
 		this.add(canvas);
 
-		Font conditionFont = new Font("VERDANA", Font.LAYOUT_LEFT_TO_RIGHT, 51);
+		Font conditionFont = new Font("VERDANA", Font.LAYOUT_LEFT_TO_RIGHT, 70);
 		Font smallFont = new Font("TAHOMA", Font.CENTER_BASELINE, 11);
-		//DecimalFormat currentCFormat = new DecimalFormat( "##0" );
-		//cTI = (int) cT;
 
 		
-		
-		//Will change to getters, and adjust positions and fonts
 		currentTemp = new JLabel("Current Temp");
 		currentTemp.setFont(conditionFont);
 		currentTemp.setForeground(Color.white);
 		canvas.setBackground(Color.darkGray);
-		canvas.add(currentTemp, 10, 30);
+		canvas.add(currentTemp, 30, 10);
 		
 		precipPercent = new JLabel("Chance of Rain: ##.# in");
 		precipPercent.setFont(smallFont);
@@ -42,47 +37,47 @@ public class CurrentConditionsPanel extends JPanel implements WeatherPanel
 		feelsLike = new JLabel("Feels Like: ##.#\u00B0F");
 		feelsLike.setFont(smallFont);
 		feelsLike.setForeground(Color.white);
-		canvas.add(feelsLike, 40, 130);
+		canvas.add(feelsLike, 45, 130);
 		
 		humidity = new JLabel("Humidity: ##%");
 		humidity.setFont(smallFont);
 		humidity.setForeground(Color.white);
-		canvas.add(humidity, 40, 150);
+		canvas.add(humidity, 50, 150);
 		
 		elevation = new JLabel("Elevation: #######ft");
 		elevation.setFont(smallFont);
 		elevation.setForeground(Color.white);
-		canvas.add(elevation, 40, 170);
+		canvas.add(elevation, 47, 170);
 		
 		highTemp = new JLabel( "High: ###.#\u00B0F");
 		highTemp.setFont(smallFont);
 		highTemp.setForeground(Color.white);
-		canvas.add(highTemp, 260, 10);
+		canvas.add(highTemp, 280, 10);
 		
 		lowTemp = new JLabel("Low: ###.#\u00B0F");
 		lowTemp.setFont(smallFont);
 		lowTemp.setForeground(Color.white);
-		canvas.add(lowTemp, 260, 30);
+		canvas.add(lowTemp, 380, 10);
 		
 		conditionIcon = new JLabel("ICON");
 		conditionIcon.setForeground(Color.white);
 		conditionIcon.setPreferredSize(new Dimension(109, 75));
-		canvas.add(conditionIcon, 260, 50);
+		canvas.add(conditionIcon, 320, 40);
 		
 		conditionText = new JLabel( "Partly Cloudy" );
 		conditionText.setFont(smallFont);
 		conditionText.setForeground(Color.white);
-		canvas.add(conditionText, 260, 130);
+		canvas.add(conditionText, 350, 120);
 		
 		windDir = new JLabel("Wind Direction: South");
 		windDir.setFont(smallFont);
 		windDir.setForeground(Color.white);
-		canvas.add(windDir, 260, 150);
+		canvas.add(windDir, 300, 140);
 		
 		windSpeed = new JLabel("Wind Speed: #.#mph");
 		windSpeed.setFont(smallFont);
 		windSpeed.setForeground(Color.white);
-		canvas.add(windSpeed, 260, 170);
+		canvas.add(windSpeed, 300, 165);
 		
 	}
 
