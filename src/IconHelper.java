2239f73629866a416fb8d48f0d131102a415f3e2
Nick Badal <badalnick@gmail.com>
2014-05-14 17:31:58 -0700

    Add image resizer helper

--- src/IconHelper.java
+++ src/IconHelper.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.HashMap;
 
 public class IconHelper
 {
-    private static HashMap<String, Icon> icons = new HashMap<String, Icon>();
+    private static HashMap<String, ImageIcon> icons = new HashMap<String, ImageIcon>();
 
     // Loop through every icon asset and add it to the hash map
     // TODO: add second hashmap for night time; check and default to day for duplicates
@@ -17,5 +19,14 @@ public class IconHelper
 
     public static Icon getIcon(String name) {
         return icons.get(name);
+    }
+
+    public static Icon getIcon(String name, int width, int height) {
+        Image img = icons.get(name).getImage();
+        BufferedImage bi = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+        Graphics g = bi.createGraphics();
+        g.drawImage(img, 0, 0, width, height, null);
+
+        return new ImageIcon(bi);
     }
 }
